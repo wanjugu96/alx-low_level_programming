da5b682f22a191dee50907a2cfcdce0bc73f5690
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-03 18:30:51 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 	float decimal;
 	long long int longlong_int;
 	long int long_integer;
-	
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(character));
 	printf("Size of a int: %zu byte(s)\n", sizeof(integer));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long_integer));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlong_int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(decimal));
 
-	return (0);}
+	return (0); }
