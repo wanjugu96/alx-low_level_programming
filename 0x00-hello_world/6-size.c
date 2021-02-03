226280c3c1e63ddd9b394f3a89902afefb6f855e
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-03 17:56:57 +0300

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@ int main(void)
 	long long int longlong_int;
 	long int long_integer;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(character));
-	printf("Size of a int: %zu byte(s)\n", sizeof(integer));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long_integer));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlong_int));
-	printf("Size of a float: %zu byte(s)\n", sizeof(decimal));
+	printf("Size of a char: %zu byte(s)", sizeof(character));
+	printf("Size of a int: %zu byte(s)", sizeof(integer));
+	printf("Size of a long int: %zu byte(s)", sizeof(long_integer));
+	printf("Size of a long long int: %zu byte(s)", sizeof(longlong_int));
+	printf("Size of a float: %zu byte(s)", sizeof(decimal));
 
 	return (0);}
