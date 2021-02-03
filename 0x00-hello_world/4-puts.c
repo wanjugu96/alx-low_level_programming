b668a07dbb9325cff8c0394678beebd768cfa0b3
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-03 16:22:37 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 int main()
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
  }
