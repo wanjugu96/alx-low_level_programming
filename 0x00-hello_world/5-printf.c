eba4f9a35af292e8a4b3d2c4eb75001388201412
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-04 10:05:44 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main(void)
-/*
- * main - print sentence
- *
- * Return:  0
+/**
+ * main - printf senetence
+ * Return: 0
  */
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
