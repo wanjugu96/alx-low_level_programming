24fc01b5d0792623c6a843838779c967ab25e9ca
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-04 19:49:32 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,17 @@
+#include <stdio.h> 
 
+int main()
+{
+    char ch = '0';
+    char chara = ',';
+    char space = ' ';
+    
+    for (ch = '0'; ch <= '9'; ch++)
+    {
+            putchar(ch);
+            putchar(chara);
+            putchar(space);
+
+    }   
+    return (0);
+}
