8a0cd760e34ef49443a429d309dedc6433a15cd0
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-04 19:34:58 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+    char *ch = "0123456789abcdef";
+    int i = 0;
 
+    for (i = 0; i <= 26; i++)
+    {
+      putchar(ch[i]);
+    }
+    return (0);
+}
