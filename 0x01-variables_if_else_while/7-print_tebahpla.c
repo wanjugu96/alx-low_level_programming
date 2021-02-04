4073e0c1145c468a89723475deb1d810cf54c0f0
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-04 18:36:00 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print alphabet in reverse
+ * Return: 0
+ */
+int main(void)
+{
+    char* ch = "abcdefghijklmnopqrstuvwxyz";
+    int i = 25;
+
+    for (i = 25; i>=0; i--)
+    {
+        putchar(ch[i]);
+    }
+    return (0);
+}
