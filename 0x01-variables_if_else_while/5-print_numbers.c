8792cf6df693e770e965d0a180736e41822c18c1
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-04 18:00:36 +0300

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+    int num = 0;
+    for (num=0; num <= 9; num++)
+    {
+        printf("%d", num);
+    }
+    return (0);
+}
