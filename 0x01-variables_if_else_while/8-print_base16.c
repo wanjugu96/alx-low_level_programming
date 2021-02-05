f7468e048f85767b6a03897990c2ea473fe6ed27
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-05 13:05:41 +0300

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - print alphabet
+ * main - print hexadecimal
  * Return: 0
  */
 int main(void)
 {
-    char *ch = "0123456789abcdef";
-    int i = 0;
-
-    for (i = 0; i <= 26; i++)
-    {
-      putchar(ch[i]);
-    }
-    return (0);
+char *ch = "0123456789abcdef";
+int i = 0;
+for (i = 0; i <= 26; i++)
+{
+putchar(ch[i]);
+}
+putchar('\n')
+return (0);
 }
