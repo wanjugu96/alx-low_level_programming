a274494d05ac16a45b7f074889ddcc810c928bae
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-05 15:08:06 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - print alphabet
- * Return: 0
- */
+* main - print alphabet
+* Return: 0
+*/
 int main(void)
 {
-    char *ch = "abcdefghijklmnopqrstuvwxyz";
-    int i = 0;
+char *ch = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
 
-    for (i = 0; i <= 26; i++)
-    {
-      putchar(ch[i]);
-    }
-    return (0);
+for (i = 0; i <= 25; i++)
+{
+putchar(ch[i]);
+}
+putchar('\n')
+return (0);
 }
