c97db9863854f4a76eb23c21add84d52dc4b5ace
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-05 12:11:42 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,20 +2,20 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - print alphabet
- * Return: 0
- */
+* main - print alphabet
+* Return: 0
+*/
 int main(void)
 {
-    char* ch = "abcdefghijklmnopqrstuvwxyz";
-    int i = 0;
+char* ch = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
 
-    for (i = 0; i <= 53; i++)
-    {
-        if (ch[i] != 'q' && ch[i] != 'e')
-        {
-            putchar(ch[i]);
-        }
-    }
-    return (0);
+ for (i = 0; i <= 53; i++)
+{
+ if (ch[i] != 'q' && ch[i] != 'e')
+{
+ putchar(ch[i]);
+}
+}
+return (0);
 }
