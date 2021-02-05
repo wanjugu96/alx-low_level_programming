c91aee5b8051ba7caff24771bdced1c8339505a6
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-05 14:26:59 +0300

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,21 +9,21 @@
  */
 int main(void)
 {
-	int num = '0';
-	int num1 = '0';
-	for (int num='0'; num<='9'; num++)
-	{		
-		for (int num1= '0'; num1 <= '9'; num1++)
-		{
-			putchar(num);
-			putchar(num1); 
-			if (num1 != '9' ||  num!='9')
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int num = '0';
+int num1 = '0';
+for (int num ='0'; num <='9'; num++)
+{		
+for (int num1 = '0'; num1 <= '9'; num1++)
+{
+putchar(num);
+putchar(num1); 
+if (num1 != '9' ||  num !='9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
