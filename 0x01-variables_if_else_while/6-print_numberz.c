ead3fd326be2550489c4d834ea6cce275ba62846
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-05 12:38:04 +0300

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,13 @@
  */
 int main(void)
 {
-	int i = 0;
-	char *nums = "0123456789";
-
-	for (i = 0; i < 10; i++)
-	{
-		putchar(nums[i]);
-	}
-	return (0);
+int i = 0;
+	
+char *nums = "0123456789";
+for (i = 0; i < 10; i++)
+{
+putchar(nums[i]);
+putchar('\n')
+}
+return (0);
 }
