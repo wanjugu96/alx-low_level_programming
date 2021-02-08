8b52095296289510f4ca0d6cb397eeab9506274b
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-08 21:42:57 +0300

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,29 @@
+//#include "holberton.h"
+#include <stdlib.h>
+#include <ctype.h>
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+//int print_last_digit(int);
+int print_last_digit(int num1)
+{
+    int num2;
+    num2= num1 % 10;
+        return num2;
+        
+}
+
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    putchar('0' + r);
+    putchar('\n');
+    return (0);
+}
