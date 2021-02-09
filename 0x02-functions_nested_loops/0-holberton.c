bbcc3c5ee039ce57c9c0de67c5aa828238a8afd1
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 16:24:54 +0300

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,16 +4,9 @@
 * main - prints holberton
 * Return: Always 0.
 */
-int holberton(void)
-{
-	char *name = "Holberton";
-	_putchar(name);
-	return (0);
-	
-}
 int main(void)
 {
-	holberton();
-	//return (0);
-	
+char *name = "Holberton";
+_putchar(name);
+return (0);
 }
