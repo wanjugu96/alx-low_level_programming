9b9c5e434096940a03459b37c5d8dbb9c75baf1f
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 09:59:15 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int add(int, int);
+int add(int x, int y)
+{
+    int z = x + y;
+        return z;
+}
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
