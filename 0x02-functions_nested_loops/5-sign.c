42725d94d3877aaa529d40c53b35b746609db728
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 17:10:28 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,24 @@
-
+#include "holberton.c"
+/**
+* print_sign- that prints the sign of a number.
+* Return:  1 if positive,0 if zero ,-1 if negative
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (0);
+}
