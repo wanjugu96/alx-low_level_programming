9f8e0330c41492130531e2edfbd6993f6623f38c
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 16:43:02 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 
 #include "holberton.h"
 #include < ctype.h >
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * _islower-check lowercase
+ * Return: what does the function return
  */
 int _islower(int c) 
 {
@@ -20,16 +18,4 @@ int _islower(int c)
         return(0);
     }
 }
-    int main(void)
-{
-    int r;
-
-    r = _islower('H');
-    _putchar(r + '0');
-    r = _islower('o');
-   _putchar(r + '0');
-    r = _islower(108);
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
-}
+    
