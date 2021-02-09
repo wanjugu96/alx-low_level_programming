d8138d9df63a76bd8bbc076bfc11c6a9e7fdb116
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 17:03:45 +0300

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,11 @@
 #include "holberton.h"
-#include <ctype.h>
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check for alphabet
+* Return: 1 if alphabet else 0
+*/
 int _isalpha(int c)
 {
-    int i;
-    i = isalpha(c);
-    if (i > 0)
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
     {
         return 1;
     }
@@ -17,19 +13,4 @@ int _isalpha(int c)
     {
         return (0);
     }
-}
-int main(void)
-{
-    int r;
-
-    r = _isalpha('H');
-    _putchar(r + '0');
-    r = _isalpha('o');
-     _putchar(r + '0');
-    r = _isalpha(108);
-     _putchar(r + '0');
-    r = _isalpha(';');
-     _putchar(r + '0');
-     _putchar('\n');
-    return (0);
 }
