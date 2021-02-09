f8210e751e46a550f99fcb82292696d356c95fc6
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 11:34:41 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 
-void print_alphabet(void)
-{
-    char alpha = 'a';
-    for (alpha= 'a'; alpha <= 'z'; alpha++)
-        _putchar(alpha);
-
-    _putchar('\n');
-
-}
 int main(void)
 {
-    print_alphabet();
-    return (0);
+char alpha = 'a';
+
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+ _putchar(alpha);
+}
+_putchar('\n');
+return (0);
 }
