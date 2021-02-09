6c152d395effaa3de920f4c93296e9de417fb9a2
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-09 16:45:12 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,19 @@
 
 #include "holberton.h"
-#include < ctype.h >
 /**
  * _islower-check lowercase
  * Return: what does the function return
  */
 int _islower(int c) 
 {
-    int i;
-    i = islower(c);
-    if (i > 0)
-    {
-        return 1;
-    }
-    else
-    {
-        return(0);
-    }
+int i;
+i = islower(c);
+if (i > 0)
+{
+return 1;
 }
-    
+else
+{
+return(0);
+}
+}   
