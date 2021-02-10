0d033e07a00bfbeee4fa3f5b165fa5c61dc83977
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-10 13:33:39 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,30 @@
-
+#include "holberton.h"
+/**
+* times_table - 9 .. times tables 
+*  return
+*/
+void times_table(void)
+{
+    int i = 0;
+    int mult = 0;
+    int num;
+    while (mult <= 9)
+    {
+    for (i = 0; i <= 9; i++)
+    {
+        num = i * mult;
+        if (num >= 10)
+        {
+            putchar('0' + (num / 10));
+            putchar('0' + (num % 10));
+        }
+        else
+        {
+            putchar(num);nn
+        }
+            
+    }
+    putchar('\n');
+    mult++;
+}
+}
