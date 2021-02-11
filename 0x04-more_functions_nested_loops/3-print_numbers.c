181d56b4b3ae7a6fdd873355b850c7d9093432f8
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 16:25:21 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9
- *
- * Return: Always 0.
- */
+* print_numbers - prints the numbers, from 0 to 9
+*
+* Return: Always 0.
+*/
 void print_numbers(void)
 {
-    int c = '0';
-    
-    for (c = '0'; c <= '9'; c++)
-    {
-        putchar(c);
-    }
-    _putchar('\n');
-    return(0);
+int c = '0';
+   
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+return(0);
 }
