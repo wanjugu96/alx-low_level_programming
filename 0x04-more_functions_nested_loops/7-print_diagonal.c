c401e2b18311d5b7f5eb4656043a3c046f2e02bb
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 21:40:34 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,33 @@
+#include "holberton.h"
 
+/**
+ * print_diagonal - print diagonal line
+ * @ int n- number tof times / is sprinted
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+    if (n <= 0)
+    {
+        putchar('\n');
+    }
+    else
+    {
+        int i = 0;
+        int j = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (j == i)
+                    putchar('//');
+                else
+                    putchar(' ');
+            }
+            putchar('\n');
+        }
+
+        return 0;
+    }
+}
