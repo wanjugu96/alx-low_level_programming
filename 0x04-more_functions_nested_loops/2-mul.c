08b05378d4307cff6f0c713c916da61d7731c3a0
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 14:53:34 +0300

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two numbers
+ * @int a:multipliers
+ * @int b:multipliers
+ * Return:return reult of multiplication.
+ */
+int mul(int a, int b)
+{
+    int x;
+    
+    x= a * b;
+    return (x);
+}
