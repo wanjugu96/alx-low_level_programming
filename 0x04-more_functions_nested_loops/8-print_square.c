43873557710cda30077385639207c525110cb029
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 22:39:16 +0300

    Update 8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,26 @@
+#include "holberton.h"
 
+/**
+ *print_square - c prints a square, followed by a new line
+ *
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+    int i = 0;
+    int j = 0;
+
+    if (size > 0)
+    {
+        for (j = 0; j < size; j++)
+        {
+            for (i = 0; i < size; i++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+    else
+        putchar('\n');
+}
