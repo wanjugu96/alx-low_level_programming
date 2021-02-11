0551ff581761008bf07dd0880ecc19f47c80dee7
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 20:00:48 +0300

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,25 @@
+#include "holberton.h"
 
+/**
+ * main - draws a straight line in the terminal
+ * @ int n- number tof times _ is sprinted
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+    if (n <= 0)
+    {
+        _putchar('\n');
+    }
+    else
+    {
+
+        int i = 0;
+        for (i = 0; i < n; i++)
+        {
+            _putchar('_');
+        }
+        putchar('\n');
+
+    }
+}
