9933b07c01e0497abec0430a0b0f78f981a74237
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 14:36:44 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
- * main - check for uppercase letters
- *
- * Return: Always 1 if c is uppercase and 0 is lower.
- */
+* main - check for uppercase letters
+*@ int c- character to check if upper
+* Return: Always 1 if c is uppercase and 0 is lower.
+*/
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z')
-    {
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
+if (c >= 'A' && c <= 'Z')
+{
+return 1;
+}
+else
+{
+return 0;
+}
 }
