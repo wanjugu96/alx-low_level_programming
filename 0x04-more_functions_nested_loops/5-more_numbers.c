06bf13b36b280c5a2c43c93e7c137a06458170fa
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 19:48:48 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,29 +7,34 @@
 */
 void more_numbers(void)
 {
-int i = 0;
-int j = 0;
-int k = 0;
-int z = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int z = 0;
+    //char c='0';
 
-while (z <= 10)
-{
-for (i = 0; i <= 14; i++)
-{
-if (i > 9)
-{
-int j = i / 10;
-int k = i % 10;
-_putchar(j + '0');
-_putchar(k + '0');
-}
-else
-{
-_putchar(i + '0');
-}
-}
-z++;
-_putchar('\n');
+    while (z <= 10)
+    {
+        for (i = 0; i <= 14; i++)
+        {
+            if (i > 9)
+            {
+                int j = i / 10;
+                int k = i % 10;
+                _putchar(j + '0');
+                _putchar(k + '0');
+
+            }
+            else
+            {
+                _putchar(i + '0');
+            }
+            //i++;
+        }
+        z++;
+        _putchar('\n');
+     
     }
    
+    
 }
