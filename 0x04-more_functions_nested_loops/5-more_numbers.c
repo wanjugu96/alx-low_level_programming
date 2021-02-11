a4e9b8935d7d23c6e9d5d9d40cc6afa5eb7bd71a
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-11 19:43:49 +0300

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,40 +1,35 @@
 #include "holberton.h"
 
 /**
- *more_numbers - prints 10 times the numbers, from 0 to 14
- *
- * Return: Always 0.
- */
+*more_numbers - prints 10 times the numbers, from 0 to 14
+*
+* Return: Always 0.
+*/
 void more_numbers(void)
 {
-    int i = 0;
-    int j = 0;
-    int k = 0;
-    int z = 0;
-    //char c='0';
+int i = 0;
+int j = 0;
+int k = 0;
+int z = 0;
 
-    while (z <= 10)
-    {
-        for (i = 0; i <= 14; i++)
-        {
-            if (i > 9)
-            {
-                int j = i / 10;
-                int k = i % 10;
-                putchar(j + '0');
-                putchar(k + '0');
-
-            }
-            else
-            {
-                putchar(i + '0');
-            }
-            //i++;
-        }
-        z++;
-        putchar('\n');
-     
+while (z <= 10)
+{
+for (i = 0; i <= 14; i++)
+{
+if (i > 9)
+{
+int j = i / 10;
+int k = i % 10;
+_putchar(j + '0');
+_putchar(k + '0');
+}
+else
+{
+_putchar(i + '0');
+}
+}
+z++;
+_putchar('\n');
     }
    
-    
 }
