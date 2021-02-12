a8e4c522b92899a74110fa81de2dfea0a650aedd
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-12 15:49:20 +0300

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,42 @@
+#include "holberton.h"
 
+/**
+ * print_triangle - function that prints a triangle.
+ *
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+    int i = 0;
+    int j = 0;
+    int k = 0;
+     
+    if (size > 0)
+    {
+        for (j = 0; j < size; j++)
+        {
+            for (i = 0; i <= size; i++)
+
+            {
+                if (i < size - j)
+                {
+                    _putchar(' ');
+
+                }
+                else
+                {
+                    _putchar('#');
+                }
+
+
+            }
+            _putchar('\n');
+        }
+    }
+    else
+    {
+        _putchar('\n');
+    }
+
+    
+}
