982b2794e92ba4465a382e04eb77f5c48816cd63
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-12 10:50:33 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void fizz_buzz(void)
+{
+    int i = 0;
+    for (i = 1; i <= 100; i++)
+    {
+        if ((i % 5 == 0 && i % 3 == 0) && (i % 5 == 0 || i % 3 == 0))
+        {
+            printf("FizzBuzz ");
+        }
+        else if (i % 5 == 0)
+        {
+            printf("Buzz ");
+        }
+        else if (i % 3 == 0)
+        {
+            printf("Fizz ");
+        }
+        else
+        {
+            printf("%d ", i);
+        }
+    }
 
+    
+}
