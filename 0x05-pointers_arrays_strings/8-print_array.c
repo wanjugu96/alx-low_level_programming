c54da36f0c38482b5cbbf2333ce2c1d3a6c8896b
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-15 21:02:15 +0300

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,16 @@
-
+#include "holberton.h"
+#include <stdio.h>
+void print_array(int* a, int n)
+{
+    int i = 0;
+    for (i = 0; i < 5; i++)
+    {
+        a[i] = *(a + i);
+        printf("%d", a[i]);
+        if (i != n-1)
+        { 
+            printf(", ");
+        }
+        
+    }
+}
