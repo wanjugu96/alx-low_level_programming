fa409e6de94b765fbfec589d1b9861adda2324d3
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-16 23:02:24 +0300

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void swap_int(int* a, int* b)
+{
+    int temp;
+    temp= *a;
+    *a = *b;
+    *b = temp;
+}
