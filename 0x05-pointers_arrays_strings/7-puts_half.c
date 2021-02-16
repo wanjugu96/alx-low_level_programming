c8def44e85eb1413b8ad8f09648528cd4593a030
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-16 12:54:05 +0300

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,32 @@
+//#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void puts_half(char* str)
+{
+    //str[] = *str;
+    int z;
+    char strr[] = { *str };
+    int i = 0;
+    int j = sizeof(strr);
+    z =  j/ 2;
+    //char z;
+    //char[] = "";
+    for (i = z; i <= j; i++)
+    {
+        str[i] = *(str + i);
+        printf("%c", str[i]);
+        
+    }
+}
+int main(void)
+{
+    char* str;
 
+    str = "0123456789";
+    puts_half(str);
+    return (0);
+}
