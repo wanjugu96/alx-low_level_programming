4303b66f1f11dd632bbca1b1a8c115e0d78bf760
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-17 00:18:17 +0300

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_rev(char* s)
+{
+    char* z;
+   // z = &s[0];
+    int count = 0;
+    int i = 0;
+    int temp;
+  
+        while (s[count] != '\0')
+        {
+            count++;
+        }
+        i = count;
+        while (i>0)
+        {
+            _putchar(s[i]);
+            i--;
+        }
+       // printf("%d", count);
+        _putchar('\n');
+}
