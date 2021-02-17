4480a0a5a27fe7026314a62434815d431445054f
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-17 08:37:24 +0300

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,17 +2,12 @@
 void _puts(char* str)
 {
     char* z;
-   // z = &s[0];
     int count = 0;
     int i = 0;
    
-    //hile (count < *z)
-   // {
-        while (str[count] != '\0')
+  while (str[count] != '\0')
         {
             _putchar(str[count]);
-
-            //rintf("\n");
             count++;
         }
         _putchar('\n');
