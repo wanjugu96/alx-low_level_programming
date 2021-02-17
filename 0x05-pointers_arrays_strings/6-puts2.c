5f8cf3d9ecfad093ed421a1d1e00de1b48691106
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-17 09:26:21 +0300

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,26 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 
+void puts2(char* str)
+{
+    int z=0;
+    int i = 0;
+ 
+    while (str[z] != '\0')
+    {
+        z++;
+    }
+    for (i = 0; i <= z; i++)
+    {
+        if (i % 2== 0)
+        {
+            _putchar(str[i]);
+        }
+       
+    }
+  _putchar('\n');
+}
