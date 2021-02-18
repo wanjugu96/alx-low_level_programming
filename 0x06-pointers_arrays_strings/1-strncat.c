37dc1b1f397a32b46159ad34a3c27927d72167b1
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-18 20:59:31 +0300

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *char* _strncat - concatenated s1
+ *
+ * Return: pointer to s1.
+ * @char* dest, char* src, int n
+ */
+char* _strncat(char* dest, char* src, int n)
+{
+    char str = "";
+    int i = 0;
+    int z = 0;
+    int k = sizeof(src);
+    while (dest[z] != '\0')
+    {
+        z++;
+    }
+    for (i=0; i< n; i++, z++)
+    {
+        dest[z] = src[i];
+    }
+    if (sizeof(src)< n)
+    {
+        dest[z] = '\0';
+    }
+    
+   
+   return dest;
+}
