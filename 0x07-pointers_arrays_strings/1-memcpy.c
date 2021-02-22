e3b31378c3d256a429628d5e3e1f0e068c9d15d7
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 17:57:15 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 /**
+ *_memcp-unction copies n bytes from memory area src to memory area dest
  * @dest -This is pointer to the destination array where the content is to be copied
  * @src: his is pointer to the source of data to be copied,
  * @n: This is the number of bytes to be copied.
