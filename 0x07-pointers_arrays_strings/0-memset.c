408cb3a022aa3df9075f63b6e5a18ec15014f904
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 15:19:05 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-"holberton.h"
+#include "holberton.h"
 /**
  * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
  * @s: This is a pointer to the block of memory to fill.
