a4af285445d6bfa6217f78caf102ed19725d1756
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 17:54:20 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,11 @@
 #include "holberton.h"
-
 /**
- * dest -This is pointer to the destination array where the content is to be copied
+ * @dest -This is pointer to the destination array where the content is to be copied
  * @src: his is pointer to the source of data to be copied,
  * @n: This is the number of bytes to be copied.
  *
  * Return: Returns a pointer to dest.
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
