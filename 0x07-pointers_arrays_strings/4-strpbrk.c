d49848968577d3e7ecba5914eccaf8833c7d8653
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 22:31:13 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,27 @@
+#include "holberton.h"
 
+/**
+ *  _strpbrk - Locate characters in string
+ * @s -C string to be scanned 
+ * @accept -C string containing the characters to match.
+ * Return: Returns a pointer to the byte in s that matches one of the bytes in accept
+ */
+char* _strpbrk(char* s, char* accept)
+{
+    int i;
+    int j;
+
+    for (i = 0; i < s[i]; i++)
+    {
+        for (j = 0; accept[j]; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                return (s + i);
+                break;
+            }
+               
+        }
+    }
+    return (0);
+}
