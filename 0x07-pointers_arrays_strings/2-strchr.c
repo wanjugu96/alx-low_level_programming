ef534e0d895bb62d472dd4dc52be23c740180dc2
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 21:12:43 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,7 +5,6 @@
  *@c - character being checked.
  * Return: Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
  */
- */
 char* _strchr(char* s, char c)
 {
     int i;
