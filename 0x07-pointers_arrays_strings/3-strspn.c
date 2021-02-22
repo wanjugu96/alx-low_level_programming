af7f0e619819e9cb36ba49c237c5706ca1ca93f0
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 22:15:16 +0300

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,30 @@
+#include "holberton.h"
+/**
+ * _strspn - Returns the length of the initial portion of str1 which consists only of characters that are part of str2.
+ * @s- string to be scanned
+ * @c- C string containing the characters to match
+ * Return: Always 0.
+ */
+unsigned int _strspn(char* s, char* accept)
+{
+    int i = 0;
+    int j = 0;
+    int z = 0;
+    unsigned int k = 0;
 
+    while (s[z] != ' ')
+    {
+        z++;
+    }
+    for (i = 0; i<z; i++)
+    {
+        for (j = 0; accept[j]; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                k++;
+            }
+        } 
+    }
+    return k;
+}
