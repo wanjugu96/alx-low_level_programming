3f1225e4a41ff875afd54484f45e25b58f687b15
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 15:16:57 +0300

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s: This is a pointer to the block of memory to fill.
+ * @b:  This is the value to be set. 
+ * @n: This is the number of bytes to be set to the value.
+ * Return: Nothing.
+ */
+char* _memset(char* s, char b, unsigned int n)
+{
+    int i = 0;
+    for (i = 0; i <n; i++)
+    {
+        *(s + i) = b;
+    }
+    return (s);
+}
