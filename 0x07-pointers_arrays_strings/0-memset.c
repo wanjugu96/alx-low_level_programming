77796a8e028e08ebc4eb0e091847a2246161dcc1
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 15:59:58 +0300

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,11 @@
  */
 char* _memset(char* s, char b, unsigned int n)
 {
-    int i = 0;
-    char* character;
-    character = b;
+  unsigned int i = 0;
+    
     for (i = 0; i <n; i++)
     {
-        *(s + i) = character;
+        *(s + i) = b;
     }
     return (s);
 }
