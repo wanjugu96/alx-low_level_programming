c281c39eba00c732397bbc439e37570e67b1d84d
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 23:49:11 +0300

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,16 +5,17 @@
  * @a -array
  * Return: Always 0.
  */
-void print_chessboard(char(*a)[8])
+
+void print_chessboard(char (*a)[8])
 {
-    int i;
-    int j;
-    
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-        {
-            _putchar(a[i][j]);
-        }
-       _putchar('\n');
-    }
+	int i, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+		{
+			_putchar(a[i][k]);
+		}
+		_putchar('\n');
+	}
+}
