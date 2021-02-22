6375ec02c8cb05c4914d4995269f82f447697f1b
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 18:30:48 +0300

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strchr - check the code for Holberton School students.
+ *@s - string to be checked for character
+ *@c - character being checked.
+ * Return: Returns a pointer to the first occurrence of the character c in the string s, or NULL if the character is not found
+ */
+char* _strchr(char* s, char c)
+{
+    int z=0;
+    int i;
+    while (s[z] != '\0')
+    {
+        z++;
+    }
+   
+    for (i = 0; i < z; i++)
+    {
+        
+        if (s[i] == c)
+        {
+            return *(s+i);
+            
+       }
+        else
+        {
+            return (0);
+        }
+        
+    }
+}
