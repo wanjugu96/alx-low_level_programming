af4f1723f87ad4e870982fefb66033bc8305abbb
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-22 17:48:22 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,16 +7,14 @@
  *
  * Return: Returns a pointer to dest.
  */
-char* _memcpy(char* dest, char* src, unsigned int n)
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-    unsigned int i ;
-    
-    for (i = 0; i < n; i++);
-    {
-        
-       dest[i] = src[i];
-    }
+	unsigned int i;
 
-    return(dest);
-
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
