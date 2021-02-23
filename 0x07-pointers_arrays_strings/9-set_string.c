9424152c4cfa819a6111a5fd7dd3833603b59ce9
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-23 23:06:11 +0300

    Create 9-set_string.c

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s- pointer value be set
+ * @c- to char value to be set
+ * Return: Always 0.
+ */
+void set_string(char*s, char *to)
+{
+*s = to;
+
+}
