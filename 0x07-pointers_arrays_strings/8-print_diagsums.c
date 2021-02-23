c423cd64ba3f79b97dedab4a7f88ef6af5af3f7f
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-23 21:42:50 +0300

    Create 8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums -  prints the sum of the two diagonals of a square matrix of integers.
+ * @a- multidimentional array
+ * @size- size of array
+ * Return: Always 0.
+ */
+void print_diagsums(int* a, int size)
+{
+    int i = 0;;
+    int sum = 0;
+    int tot = size * size;
+    int interval = size+1;
+    int interval2 = size - 1;
+    int j = size - 1;
+    int sum2 = 0;
+
+    while (i < tot)
+    {
+        sum = sum + a[i];
+        i = i + interval;
+    }
+        while (j <= tot-size)
+    {
+        sum2 = sum2 + a[j];
+        j = j + interval2;
+    }
+    printf("%d, %d\n", sum, sum2);
+}
