68368792eee5c12cfa33129635b533d87889dcca
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-24 21:09:24 +0300

    Update 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,1 +1,16 @@
-
+#include "holberton.h"
+/**
+ * factorial - calculatge factorial of a number
+ * @n -int to get factorial for
+ * Return: returns the factorial of a given number.
+ */
+int factorial(int n)
+{
+    if (n == 0) 
+        return (1);
+  
+   if (n < 0)
+        return (-1);
+        
+        return (factorial(n - 1) * n);
+}
