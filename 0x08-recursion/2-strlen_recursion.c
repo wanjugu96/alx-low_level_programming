e550f0313d2635a04c3f8fe5a34a7fbdb5c523fd
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-24 20:04:24 +0300

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,18 @@
+#include "holberton.h"
 
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s -string to be counted lenghth
+ * Return: lenghth of string.
+ */
+int _strlen_recursion(char *s)
+{
+    int k = 0;
+    int i;
+  
+    while (s[k] != '\0')
+    {
+        k++;
+    }
+    return k;
+}
