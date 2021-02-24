382620381c0c16b6bc8f06c84d6d27f964a5b4c4
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-24 22:14:04 +0300

    Update 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,16 @@
+#include "holberton.h"
 
+/**
+ * _pow_recursion -  value of x raised to the power of y
+ * @x- base value
+ * @y- power tobe multiplied by
+ * Return: x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+    if (y == 0)
+        return (1);
+    return(_pow_recursion(x, y-1) * x);
+}
