fdeb51d3c607cd2db0ac94c1556e32e653ebe381
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-25 13:16:24 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,18 @@
+#include "holberton.h"
 
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char* s)
+{
+    int  i;
+
+   for(i=0; *(s + i) != '\0'; i++)
+       
+    {
+        putchar(s[i]);
+    }
+   putchar('\n');
+}
