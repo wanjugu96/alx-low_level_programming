348964d31ec56debc42448be15e78821227105c8
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-02-25 16:27:18 +0300

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,15 @@
-
+#include "holberton.h"
+/**
+ *_print_rev_recursion- reverses a string
+ * @s - string to be reversed.
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char* s)
+{
+    if (*s == '\0')
+    {
+        return;
+    }
+    _print_rev_recursion(s + 1);
+    _putchar(*s);
+}
