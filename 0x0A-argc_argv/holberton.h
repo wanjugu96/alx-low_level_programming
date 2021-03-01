5a69744dbd5863769fc9fa98bac3993345d7b474
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-01 23:52:54 +0300

    Update holberton.h

--- 0x0A-argc_argv/holberton.h
+++ 0x0A-argc_argv/holberton.h
@@ -1,3 +1,4 @@
 int main(int argc __attribute__((unused)), char* argv[]);
 int main(int argc,char *argv[]__attribute__((unused)));
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
