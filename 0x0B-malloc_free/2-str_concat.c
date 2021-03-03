7b9768c2e603511f6c96a06be91ceb047a12098f
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-03 21:15:48 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,57 +2,39 @@
 #include <stdlib.h>
 
 /**
- * str_concat -concatinates two strings
- * @s1 -string to concatenate
- * @ s2 -string to concatenate at the end of s1
- * Return: pointer to concatenated string
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: pointer to newly allocated space in memory, or NULL if error
  */
-char* str_concat(char* s1, char* s2)
+char *str_concat(char *s1, char *s2)
 {
-   unsigned int z =0;
-    unsigned int k =0;
-    unsigned int i;
-    char* ar;
-    unsigned int j;
-    
-    if (s1 == NULL)
-    {
-        z = 0;
-    }
-    else
-    {
-        while (s1[z] != '\0')
-        {
-            z++;
-        }
-    }
-    if (s2 == NULL)
-    {
-        k == 0;
-    }
-    else
-    {
-        while (s2[k] != '\0')
-        {
-            k++;
-        }
-    }
-    j = k + z + 1;
-    ar = malloc(j * sizeof(char));
-    if (ar == NULL)
-    {
-        return (NULL);
-    }
-    for (i = 0; i < z; i++)
-    {
-        ar[i] = s1[i];
-    }
-    for (i = 0; i < k; i++)
-    {
-        ar[i + z] = s2[i];
-    }
-    ar[z + k] = '\0';
+	unsigned int i, j, k, l;
+	char *s;
 
-    return (ar);
-    free(ar);
+	if (s1 == NULL)
+		i = 0;
+	else
+	{
+		for (i = 0; s1[i]; i++)
+			;
+	}
+	if (s2 == NULL)
+		j = 0;
+	else
+	{
+		for (j = 0; s2[j]; j++)
+			;
+	}
+	k = i + j + 1;
+	s = malloc(k * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (l = 0; l < i; l++)
+		s[l] = s1[l];
+	for (l = 0; l < j; l++)
+		s[l + i] = s2[l];
+	s[i + j] = '\0';
+	return (s);
 }
