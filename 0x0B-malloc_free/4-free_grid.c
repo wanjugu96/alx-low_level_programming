78e9ffb5f1589c16e300626bf08821e8a05b90b6
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-03 21:44:11 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,12 +1,20 @@
-void free_grid(int** grid, int height)
+#include "holberton.h"
+/**
+ * free_grid - frees memory created for a grid 
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
 {
   int i;
-  if(grid==NULL || height<=0)
+  
+  if(grid == NULL || height<=0)
   {
     for(i = 0; i < height; i++)
-    {
       free grid[i];
-    }
     free (grid);
   }
 }
