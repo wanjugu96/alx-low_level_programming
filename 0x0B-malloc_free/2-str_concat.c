37d2fd3b8b286f5bb3d74faaed0763972ae51796
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-03 20:37:23 +0300

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,13 +9,12 @@
  */
 char* str_concat(char* s1, char* s2)
 {
-    int z = 0;
-    int k = 0;
-    
-    int i;
+   unsigned int z =0;
+    unsigned int k =0;
+    unsigned int i;
     char* ar;
-    int j;
-
+    unsigned int j;
+    
     if (s1 == NULL)
     {
         z = 0;
