aea844d33e93939b16873c10465df245f78a4cb8
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-03 22:02:36 +0300

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,15 +8,21 @@
  * Return: Nothing.
  */
 
-void free_grid(int **grid, int height)
+void free_grid(int** grid, int height)
 {
-  int i;
-  
-  if(grid == NULL || height<=0)
-    return;
-  
-    for(i = 0; i < height; i++)
-      free (grid[i]);
-    free (grid);
-  
+    int i;
+
+    if (grid == NULL || height <= 0)
+    {
+        return;
+    }
+        
+
+    for (i = 0; i < height; i++)
+    {
+        free(grid[i]);
+    }
+
+    free(grid);
+
 }
