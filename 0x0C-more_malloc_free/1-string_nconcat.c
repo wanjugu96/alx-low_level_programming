91a22f90dd67a7b0bdd417afbf62c21bea4301d6
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-04 19:26:09 +0300

    Create 1-string_nconcat.c

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - check the code for Holberton School students.
+ * @s1 -string 1
+ * @s2 -string 2
+ * Return: returned pointer shall point to a newly allocated space in memory, .
+ */
+char* string_nconcat(char *s1, char *s2, unsigned int n)
+{
+    unsigned int i, j, k,sum = 0;
+    char *ar;
+
+    if (s1 == NULL)
+    {
+        j = 0;
+    }
+    else
+    {
+        for (j = 0; s1[j]; j++)
+            ;
+    }
+    if (s2 == NULL)
+    {
+        k = 0;
+    }
+    else
+    {
+        for (k = 0; s2[k]; k++)
+            ;
+    }
+
+
+    sum = j + k + 1;
+    ar = malloc(sum * sizeof(char));
+     
+    if (ar == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < j; i++)
+    {
+        ar[i] = s1[i];
+    }
+    for (i = 0; i <= n; i++)
+    {
+        ar[i+j] = s2[i];
+    }
+    ar[j+n] = '\0';
+    return (ar);
+    free(ar);
+}
