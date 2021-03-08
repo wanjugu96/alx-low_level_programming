b90e103dfdae1fb664d906c5189992576b84c6a3
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-08 19:15:26 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,10 +4,10 @@
  * @age: Second member
  * @owner: Third member
  *
- * Description: Longer description
+ * Description: struct that describes dogs age name and owner
  */
 struct dog {
-	char* name;
-	char* owner;
+	char *name;
+	char *owner;
 	float age;
 };
