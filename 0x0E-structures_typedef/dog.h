e26660a6097e4f3dca22a3ec0badf26b17d2e15a
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-08 19:09:25 +0300

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,13 @@
-
+/**
+ * struct dog - creates a new dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog {
+	char* name;
+	char* owner;
+	float age;
+};
