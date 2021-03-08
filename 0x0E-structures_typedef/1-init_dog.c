056aae381d19e5bc48aa2ce28b86c7549ed125bf
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-08 20:12:06 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,11 @@
+#include "dog.h"
 
+void init_dog(struct dog* d, char* name, float age, char* owner)
+{
+    if (d) {
+        d->name =name;
+        d->owner =owner;
+        d->age =age;
+}
+   
+}
