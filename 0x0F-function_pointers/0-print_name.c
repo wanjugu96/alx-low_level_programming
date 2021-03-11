ec2fa3e2e1336af4c42679c239e6d4a8abf13b59
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-11 13:26:00 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,11 +6,8 @@
  *
  * Return: void
  */
-
-void print_name(char* name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
-    if (name != NULL)
-    {
-        f(name);
- }
- }
+	if (name != NULL && f != NULL)
+		f(name);
+}
