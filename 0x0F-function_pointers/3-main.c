0678eee404bfcf4b2a75115944b91ae84268fb67
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-11 12:50:37 +0300

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,23 +4,17 @@
 
 int main (int argc, char *argv[])
 {
-
-      
-
  int x=atoi(argv[3]);
 int y= atoi(argv[1]);
- printf("argc =%d\n",argc);
+
  if (argc !=4)
     {
         printf("ERROR1\n");
         exit(98);
     }
-  printf("x is =%d\n",x);
-  printf("xrgv3 is =%s\n",argv[1]);
-  printf("y is =%d\n",y);
-   printf("yrgv2 is =%s\n",argv[3]);
- printf("answer is :%d\n",(get_op_func(argv[2]))(x,y));
+ 
+ printf("%d\n",(get_op_func(argv[2]))(x,y));
 
- return(4);
+ return(0);
 }
 
