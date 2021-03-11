bf51e54f3ee7034508751b8abf72ba3b984d7bf2
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-11 12:41:51 +0300

    Update 3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,26 +2,24 @@
 #include <stdlib.h>
 #include "3-calc.h"
 #include <string.h>
-int (*get_op_func(char* s))(int, int)
+int (*get_op_func(char *s))(int, int)
 {
-    
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
-	i=0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-while (ops[i].op != NULL)
+	i = 0;
+	while (ops[i].op != NULL)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			break;
 		i++;
 	}
-	return (ops[i].f);    
+	return (ops[i].f);
 }
-
