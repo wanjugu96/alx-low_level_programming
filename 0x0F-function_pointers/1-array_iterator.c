904d8a065112c3039700fa17ef10246e5ac9f7e3
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-11 13:14:27 +0300

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
 void array_iterator(int* array, size_t size, void (*action)(int))
 {
     size_t i = 0;
-    while((array != NULL) && (action != NULL))
+    if ((array != NULL) && (action != NULL))
     {
     while (i < size)
     {
