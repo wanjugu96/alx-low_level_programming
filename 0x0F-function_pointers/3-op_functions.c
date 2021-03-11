47fc9d8c03b91fc832cc5ee677dbb2c3c29264d3
wanjugu Mung'au <wanjugu.mung'au@strathmore.edu>
2021-03-11 12:23:38 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+int op_add(int a, int b)
+{
+  int x=a + b;
+  return (x);
+}
+int op_sub(int a, int b)
+{
+  int y=abs(a - b);
+
+  return (y);
+}
+int op_mul(int a, int b)
+{
+  int x=a * b;
+
+  return (x);
+}
+int op_div(int a, int b)
+{
+  int x=a / b;
+
+  return (x);
+}
+int op_mod(int a, int b)
+{
+  int x=a % b;
+  return (x);
+}
 
